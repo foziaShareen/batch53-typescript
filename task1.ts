4c9efbfc90f745473dec6f962b4789e9c767d418
foziaShareen <foziashareen1@gmail.com>
2023-12-09 10:18:59 +0500

    Update task1.ts
    updated code

--- task1.ts
+++ task1.ts
@@ -1,77 +1,71 @@
 import inquirer from 'inquirer';
-import showBanner from "node-banner";
-import gradient from "gradient-string";
+import showBanner from 'node-banner';
+import { calculateTotalBill, calculateDiscount } from './functions.js';
+
 (async () => {
-  await showBanner('shopping', 'This app can perform choices of grocery ,bill,discount',"red","blue");
+  await showBanner('Shopping', 'shopping,bill,discount', 'red', 'blue');
 })();
+
 async function Shopping() {
-  console.log('Welcome to the Supermarket\n');
+  let condition = true;
 
-  // Scenario 1 - Buying Groceries
-  console.log('=== Scenario 1: Buying Groceries ===');
+  while (condition) {
+    const { groceryType } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'groceryType',
+        message: 'Do you want to buy fruits or vegetables?',
+        choices: ['Fruits', 'Vegetables']
+      },
+    ]);
 
-  const groceryChoice = await inquirer.prompt({
-    type: 'list',
-    name: 'groceryType',
-    message: 'Do you want to buy fruits or vegetables?',
-    choices: ['Fruits', 'Vegetables'],
-  });
+    let items: string[] = [];
+    let prices: number[] = [];
 
-  let items: string[] = [];
-  let prices: number[] = [];
+    if (groceryType === 'Fruits') {
+      items = ['Apple', 'Banana', 'Melon'];
+      prices = [12.5, 8.75, 20.0];
+    } else {
+      items = ['Onion', 'Tomato', 'Carrot'];
+      prices = [13, 11, 12];
+    }
 
-  if (groceryChoice.groceryType === 'Fruits') {
-    items = ['Apple', 'Banana', 'Melon'];
-    prices = [12.5, 8.75, 20.0];
-  } else {
-    items = ['Onion', 'Tomato', 'Carrot'];
-    prices = [13, 11, 12];
+    const { selectedItems, paymentMethod } = await inquirer.prompt([
+      {
+        type: 'checkbox',
+        name: 'selectedItems',
+        message: `Select ${groceryType.toLowerCase()}:`,
+        choices: items.map(item => ({ name: item }))
+      },
+      {
+        type: 'list',
+        name: 'paymentMethod',
+        message: 'Select a payment method:',
+        choices: ['Credit Card', 'Debit Card', 'Cash']
+      },
+    ]);
+
+    const total = calculateTotalBill(items, prices, selectedItems);
+    const discount = calculateDiscount(total);
+
+    console.log(`Total Bill: ${total}`);
+    console.log(`Discount: ${discount}`);
+
+    // Handle the rest of your logic based on user input
+
+    const { again } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'again',
+        message: 'Do you want to shop again?',
+        default: true
+      },
+    ]);
+
+    condition = again;
   }
-
-  const selectedItems = await inquirer.prompt({
-    type: 'checkbox',
-    name: 'selectedItems',
-    message: `Select ${groceryChoice.groceryType.toLowerCase()}:`,
-    choices: items,
-  });
-
-  let totalBill = 0;
-  selectedItems.selectedItems.forEach((item:string) => {
-    const index = items.indexOf(item);
-    totalBill += prices[index];
-  });
-
-  console.log(`Total Bill: RS:${totalBill.toFixed(2)}\n`);
-
-  // Scenario 2 - Checking Discounts
-  console.log('=== Scenario 2: Checking Discounts ===');
-
-  if (totalBill > 10) {
-    const discountPercentage = 10;
-    const discountAmount = (totalBill * discountPercentage) / 100;
-    const discountedTotal = totalBill - discountAmount;
-
-    console.log(`You qualify for a ${discountPercentage}% discount!`);
-    console.log(`Discounted Total: RS:${discountedTotal.toFixed(2)}\n`);
-    totalBill = discountedTotal;
-  }
-
-  // Scenario 3 - Checkout Process
-  console.log('=== Scenario 3: Checkout Process ===');
-
-  const paymentMethod = await inquirer.prompt({
-    type: 'list',
-    name: 'paymentMethod',
-    message: 'Select a payment method:',
-    choices: ['Credit Card', 'Debit Card', 'Cash'],
-  });
-
-  console.log(`You have chosen to pay with ${paymentMethod.paymentMethod}.`);
-  console.log('Thank you for shopping with us!');
 }
-
 
 setTimeout(() => {
   Shopping();
-  
-}, 1000);
+}, 10000);
